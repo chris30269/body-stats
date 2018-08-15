5536e7ff46b027293cef50e992651af1d2f6cf43
Ernst, Chris R <Chris.Ernst@elavon.com>
2018-08-15 15:18:54 -0400

    yay squats

--- squat.js
+++ squat.js
@@ -1,5 +1,5 @@
-function lbm(){
-	var svg = d3.select("#lbm .container svg"),
+function squat(){
+	var svg = d3.select("#squat .container svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
@@ -20,52 +20,43 @@ var line = d3.line()
 var prevPrevVal = 0;
 var prevVal = 0;
 var curVal = 0
-var lineAvg = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d,i){
-    	if (i == 0) {
-	      prevPrevVal  = y(d.weight);
-	      prevVal = y(d.weight);
-	      curVal =  y(d.weight);
-	  } else if (i == 1) {
-	      prevPrevVal = prevVal;
-	      prevVal = curVal;
-	      curVal = (prevVal + y(d.weight)) / 2.0;
-	  } else {
-	      prevPrevVal = prevVal;
-	      prevVal = curVal;
-	      curVal = (prevPrevVal + prevVal + y(d.weight)) / 3.0;
-	  }
-    	return curVal;
-    });
 
-d3.csv("weight.csv", function(data) {
+d3.csv("squat.csv", function(data) {
 // if (error) throw error;
 // console.log(data);
-data.forEach(function(data, i) {
-	data.date = parseTime(data.date);
-	data.weight = +(data.weight-(data.weight*(data.fat/100.0)));
+var groupedData = d3.nest()
+  .key(function(d){return d.date;})
+  .rollup(function(d){
+    return d3.sum(d, function(g){
+      return g.weight/(1.0278-(.0278*g.reps));});
+  })
+  .entries(data);
+groupedData.forEach(function(d){
+  d.date = parseTime(d.key);
+  d.weight = +d.value;
 });
-var beginning = 0;
-for (var i = 0; i <= 29; i++) {
-	beginning += data[i].weight;
-	// console.log(data[i].weight);
-};
-beginning = beginning/30.0;
-// console.log(beginning);
-var end = 0;
-for (var i = (data.length-1); i >= (data.length-30); i--) {
-	end += data[i].weight;
-	// console.log(data[i].weight);
-};
-end = end/30.0;
-// console.log(end);
-var result = end-beginning;
-result = Number.parseFloat(result).toPrecision(3);
-d3.select("#lbmChange").html(result+"lbs");
+// console.log(data);
+// console.log(groupedData);
+// var beginning = 0;
+// for (var i = 0; i <= 29; i++) {
+// 	beginning += data[i].weight;
+// 	// console.log(data[i].weight);
+// };
+// beginning = beginning/30.0;
+// // console.log(beginning);
+// var end = 0;
+// for (var i = (data.length-1); i >= (data.length-30); i--) {
+// 	end += data[i].weight;
+// 	// console.log(data[i].weight);
+// };
+// end = end/30.0;
+// // console.log(end);
+// var result = end-beginning;
+// result = Number.parseFloat(result).toPrecision(3);
+// d3.select("#lbmChange").html(result+"lbs");
 
-x.domain(d3.extent(data, function(d) { return d.date; }));
-y.domain(d3.extent(data, function(d) { return d.weight; }));
+x.domain(d3.extent(groupedData, function(d) { return d.date; }));
+y.domain(d3.extent(groupedData, function(d) { return d.weight; }));
 
 g.append("g")
   .attr("transform", "translate(0," + height + ")")
@@ -82,23 +73,14 @@ g.append("g")
   .text("Lean Body Mass (lbs)");
 
 g.append("path")
-  .datum(data)
+  .datum(groupedData)
   .attr("fill", "none")
   .attr("stroke", "steelblue")
   .attr("stroke-linejoin", "round")
   .attr("stroke-linecap", "round")
   .attr("stroke-width", 1.5)
-  .attr("opacity", .2)
   .attr("d", line);
 
-g.append("path")
-  .datum(data)
-  .attr("fill", "none")
-  .attr("stroke", "steelblue")
-  .attr("stroke-linejoin", "round")
-  .attr("stroke-linecap", "round")
-  .attr("stroke-width", 1.5)
-  .attr("d", lineAvg);
 });
 
 
